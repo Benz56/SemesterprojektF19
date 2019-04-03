63d8ca89623527344cc015e8fd89c6d2c67010ba
Dan Trong Thai <dthai18@student.sdu.dk>
2019-04-03 13:49:14 +0200

    Start 1.1
    Co-Authored-By: jacobglumby1 <jacobglumby1@users.noreply.github.com>

--- src/semesterprojektf19/domain/Diary.java
+++ src/semesterprojektf19/domain/Diary.java
@@ -1,5 +1,14 @@
 package semesterprojektf19.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Diary {
-    
+
+    private List<DiaryNote> list;
+
+    public Diary() {
+        list = new ArrayList<>();
+        
+    }
 }
--- src/semesterprojektf19/domain/DiaryNote.java
+++ src/semesterprojektf19/domain/DiaryNote.java
@@ -5,12 +5,16 @@ import java.util.Date;
 public class DiaryNote {
 
     private String note;
+    private int workerID; // Kan fjernes hvis det ikke passer med logning af systemet.
     private Date d;
 
-    public DiaryNote(String note) {
+    public DiaryNote(String note, int workerID) {
         this.note = note;
+        this.workerID = workerID;
         Date d = new Date();
     }
+    
+    
     
     
 }
