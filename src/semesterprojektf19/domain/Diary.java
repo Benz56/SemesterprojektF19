225d095a4a12cda26cd319e5ed452e54cd3f2632
JacobGlumby1 <43093373+JacobGlumby1@users.noreply.github.com>
2019-04-03 15:58:54 +0200

    added notes
    Co-Authored-By: dthai18 <dthai18@users.noreply.github.com>

--- src/semesterprojektf19/domain/Diary.java
+++ src/semesterprojektf19/domain/Diary.java
@@ -11,15 +11,22 @@ public class Diary {
         list = new ArrayList<>();
     }
     
+   
+    // TODO implementer at gemme i fil
+    // TODO implementer i UI.
+    
+    
     public static void main(String[] args) {
         
         Diary d1 = new Diary();
         
         DiaryNote n1 = new DiaryNote("En note her", 1);
         DiaryNote n2 = new DiaryNote("En anden note", 2);
+        DiaryNote n3 = new DiaryNote("En tredje note", 3);
         
         d1.list.add(n1);
         d1.list.add(n2);
+        d1.list.add(n3);
         
         System.out.println(d1.list.toString());
         
