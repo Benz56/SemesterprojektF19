9c380e53b1c81d172d17009ce13f76ed7aa28bc5
Dan Trong Thai <dthai18@student.sdu.dk>
2019-04-03 15:01:40 +0200

    Good Hotbed, only needs GUI implementation
    Co-Authored-By: jacobglumby1 <jacobglumby1@users.noreply.github.com>

--- src/semesterprojektf19/domain/DiaryNote.java
+++ src/semesterprojektf19/domain/DiaryNote.java
@@ -6,15 +6,32 @@ public class DiaryNote {
 
     private String note;
     private int workerID; // Kan fjernes hvis det ikke passer med logning af systemet.
-    private Date d;
+    private Date date;
 
     public DiaryNote(String note, int workerID) {
         this.note = note;
         this.workerID = workerID;
-        Date d = new Date();
+        Date date = new Date();
     }
     
-    
-    
+        public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getWorkerID() {
+        return workerID;
+    }
+
+    public void setWorkerID(int workerID) {
+        this.workerID = workerID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
     
 }
--- src/semesterprojektf19/domain/Topic.java
+++ src/semesterprojektf19/domain/Topic.java
@@ -1,5 +1,7 @@
 package semesterprojektf19.domain;
 
 public enum Topic {
-    EXAMPLE;
+    CRITICAL, NON_CRITICAL, PERSONAL, REPRESENTATION;
+    
+    // Tilføj gerne flere "Emner", der skal fungere som filters for at give overblik over mange dagbøger.
 }
