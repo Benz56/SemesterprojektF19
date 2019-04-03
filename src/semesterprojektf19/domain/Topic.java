3f3c2eaa44b7f1cef919287314486d9ebb09a5dd
Dan Trong Thai <dthai18@student.sdu.dk>
2019-04-03 14:58:05 +0200

    1.2 (Only needs GUI Implementation)

--- src/semesterprojektf19/domain/Topic.java
+++ src/semesterprojektf19/domain/Topic.java
@@ -1,5 +1,6 @@
 package semesterprojektf19.domain;
 
 public enum Topic {
-    EXAMPLE;
+    CRITICAL, NON_CRITICAL, PERSONAL, REPRESENTATION; 
+    // Tilføj gerne flere "Emner", der skal være med til at identificere de forskellige dagbøger.
 }
