8f071201980e6f26ba3c79c716be45df92310a7e
Dan Trong Thai <dthai18@student.sdu.dk>
2019-04-03 14:58:57 +0200

    Revert "1.2 (Only needs GUI Implementation)"
    This reverts commit 3f3c2eaa44b7f1cef919287314486d9ebb09a5dd.

--- src/semesterprojektf19/domain/DiaryNote.java
+++ src/semesterprojektf19/domain/DiaryNote.java
@@ -6,33 +6,15 @@ public class DiaryNote {
 
     private String note;
     private int workerID; // Kan fjernes hvis det ikke passer med logning af systemet.
-    private Date date;
+    private Date d;
 
     public DiaryNote(String note, int workerID) {
         this.note = note;
         this.workerID = workerID;
-        Date date = new Date();
+        Date d = new Date();
     }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public int getWorkerID() {
-        return workerID;
-    }
-
-    public void setWorkerID(int workerID) {
-        this.workerID = workerID;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
+    
+    
     
     
 }
--- src/semesterprojektf19/domain/Topic.java
+++ src/semesterprojektf19/domain/Topic.java
@@ -1,6 +1,5 @@
 package semesterprojektf19.domain;
 
 public enum Topic {
-    CRITICAL, NON_CRITICAL, PERSONAL, REPRESENTATION; 
-    // Tilføj gerne flere "Emner", der skal være med til at identificere de forskellige dagbøger.
+    EXAMPLE;
 }
