2a5ce61346b7f43fcd0b65e89e760f6dd87f91cb
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2019-05-03 13:08:54 +0200

    Introduced connection to database.

--- src/semesterprojektf19/persistence/Postgres.java
+++ src/semesterprojektf19/persistence/Postgres.java
@@ -16,29 +16,29 @@ public class Postgres {
     public static void main(String[] args) {
         try {
             Class.forName("org.postgresql.Driver");
-        }
-        catch (java.lang.ClassNotFoundException e) {
+        } catch (java.lang.ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
-        String url = "jdbc:postgres://jwzwncwf:Hl-A8_Bb93HTAXJuBjDXRfPJtyU64vDL@manny.db.elephantsql.com:5432/jwzwncwf";
+        //url syntax for database connection: (driver : sqltype :// server : port/)
+        String url = "jdbc:postgresql://manny.db.elephantsql.com:5432/";
         String username = "jwzwncwf";
         String password = "Hl-A8_Bb93HTAXJuBjDXRfPJtyU64vDL";
 
         try {
             Connection db = DriverManager.getConnection(url, username, password);
-            Statement st = db.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM people");
-            while (rs.next()) {
-                System.out.print("Column 1 returned ");
-                System.out.println(rs.getString(2));
-                System.out.print("Column 2 returned ");
-                System.out.println(rs.getString(3));
-            }
-            rs.close();
-            st.close();
-            }
-        catch (java.sql.SQLException e) {
+            System.out.println("Connected to database@" + url);
+//            Statement st = db.createStatement();
+//            ResultSet rs = st.executeQuery("SELECT * FROM account");
+//            while (rs.next()) {
+//                System.out.print("Column 1 returned ");
+//                System.out.println(rs.getString(1));
+//                System.out.print("Column 2 returned ");
+//                System.out.println(rs.getString(2));
+//            }
+//            rs.close();
+//            st.close();
+        } catch (java.sql.SQLException e) {
             System.out.println(e.getMessage());
         }
     }
